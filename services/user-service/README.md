5f1e628599e741f191a0309cd411a5bf164dfef6
João Pedro de Lima <48960188+jotalima72@users.noreply.github.com>
2025-01-16 18:55:46 -0300

    user-service initial commit

---
+++ services/user-service/README.md
@@ -0,0 +1,7 @@
+# User Service
+
+A microservice within the library management system responsible for handling user-related operations including:
+
+- User profile management 
+- User account preferences
+- User book preferences
---
+++ services/user-service/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://rabbitmq:5672'],
+        queue: 'library_queue',
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
